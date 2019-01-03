dd30a806918d8b760449b1e79ed8250aeb0ea00c
Ally Conway <avconway@hotmail.co.uk>
2019-01-03 23:26:43 +0000

    Adds store balance and method to add to inventory

--- record_store/models/store.js
+++ record_store/models/store.js
@@ -1,7 +1,12 @@
-const Store = function(name, city){
+const Store = function(name, city, balance){
   this.name = name;
   this.city = city;
+  this.balance = balance;
   this.inventory = [];
 }
+
+Store.prototype.addRecord = function(record) {
+  this.inventory.push(record)
+};
 
 module.exports = Store;
--- record_store/specs/store_spec.js
+++ record_store/specs/store_spec.js
@@ -1,12 +1,17 @@
 const assert = require('assert');
 const Store = require('../models/store.js');
+const Record = require('../models/record.js');
 
 let store;
+let record1;
+let record2;
 
 describe('Store', function(){
 
   beforeEach(function(){
-    store = new Store('ABC Records', 'Glasgow')
+    store = new Store('ABC Records', 'Glasgow', 25);
+    record1 = new Record('Errors', 'Have Some Faith in Magic', 'Electronica', 8);
+    record2 = new Record('Jeff Buckley', 'Grace', 'Rock', 10)
   });
 
   it('should have a name', function(){
@@ -22,6 +27,18 @@ describe('Store', function(){
   it('should have an inventory', function(){
     const actual = store.inventory;
     assert.deepStrictEqual(actual, [])
+  });
+
+  it('should have a balance', function(){
+    const actual = store.balance;
+    assert.strictEqual(actual, 25)
+  });
+
+  it('should be able to add records to its inventory', function(){
+    store.addRecord(record1);
+    store.addRecord(record2);
+    const actual = store.inventory;
+    assert.deepStrictEqual(actual, [record1, record2])
   });
 
 })
